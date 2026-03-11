29a366c77845e57ef800f62000b58ef771e5a70f
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-03-11 11:05:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-import { PGE } from './engine.js';
-import { ecs } from './modules/ecs.js';
-import { combat } from './modules/combat.js';
-
-const Game = (() => {
-  const canvas = document.getElementById('game');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 320; canvas.height = 240;
-  
-  const init = async () => {
-    PGE.run();  // Glyph physics → ECS
-    gameLoop();
-  };
-  
-  const gameLoop = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ecs.update();
-    combat.render(ctx);
-    requestAnimationFrame(gameLoop);
-  };
-  
-  return { init };
-})();
-
-Game.init();
+const gameLoop = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  render.renderWorld();  // Glyph worlds
+  ecs.update();
+  combat.render(ctx);
+  requestAnimationFrame(gameLoop);
+};
